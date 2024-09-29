b177f9bce065e36cd91083acd9dc72d00e94cd0d
Santatra <razafindratsimba34@gmail.com>
2024-09-29 21:33:14 +0300

    WIP

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -2,24 +2,42 @@ provider "lxd" {
   accept_remote_certificate = true
 
   remote {
-    name    = "test"
-    address = "localhost"
+    name    = "local"
+    address = "127.0.0.1"
     port    = "8443"
     scheme  = "https"
-    password = "test123"
+    token   = var.lxc_token
   }
 
 }
 
-resource "lxd_instance" "instance1" {
-  name  = "instance1"
-  image = "ubuntu-daily:22.04"
+resource "lxd_network" "cluster_network" {
+  name = "cluster_network"
 
   config = {
-    "boot.autostart" = true
+    "ipv4.address" = "10.150.19.1/24"
+    "ipv4.nat"     = "true"
+    "ipv6.address" = "fd42:474b:622d:259d::1/64"
+    "ipv6.nat"     = "true"
+  }
+}
+
+resource "lxd_instance" "node1" {
+  name  = "node1"
+  image = "ubuntu:22.04"
+  type  = "virtual-machine"
+
+  device {
+    name = "eth0"
+    type = "nic"
+    properties = {
+      nictype = "bridged"
+      network  = "${lxd_network.cluster_network.name}"
+    }
   }
 
   limits = {
-    cpu = 2
+    memory = "1GiB"
   }
+
 }
